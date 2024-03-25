c543cc3546af118276e86cc8606fb68340fc516c
KotedKW <106537189+KotedKW@users.noreply.github.com>
2024-03-25 15:05:32 +0100

    Oui

--- test_calcul_hashage.py
+++ test_calcul_hashage.py
@@ -1,5 +1,6 @@
 import requests
 import pytest
+import hashlib
 
 def recup_page(url):
     # Obtenir le contenu de la page
@@ -20,3 +21,22 @@ def test_recup_page():
     
     # Vérification du code d'état HTTP
     assert result.status_code == 200
+
+
+def calculer_hachage(data):
+    sha256_hash = hashlib.sha256()
+    sha256_hash.update(data.encode('utf-8'))
+    return sha256_hash.hexdigest()
+
+def test_calculer_hachage():
+    # Données d'entrée
+    data = "Hello, World!"
+
+    # Résultat attendu
+    expected_hash = "dffd6021bb2bd5b0af676290809ec3a53191dd81c7f70a4b28688a362182986f"
+
+    # Calcul du hachage
+    result = calculer_hachage(data)
+
+    # Vérification
+    assert result == expected_hash
