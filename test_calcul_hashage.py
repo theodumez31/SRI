e42fc4cee26b6c558fa0ba722acafa34675b552b
KotedKW <106537189+KotedKW@users.noreply.github.com>
2024-03-25 14:45:30 +0100

    Test du calcul Hashage

--- test_calcul_hashage.py
+++ test_calcul_hashage.py
@@ -1,19 +1,43 @@
 import requests
- 
-from calcul_hashage import mettre_integrity
+import pytest
+import hashlib
 
-def verifier_lien_internet(url):
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            return 1  # Code d'erreur 1 = OK
-        else:
-            return 2  # Code d'erreur 2 = Erreur de récupération de la page
-    except Exception as e:
-        print(f"Erreur : {e}")
-        return 3  # Code d'erreur 3 = Échec de la requête HTTP
- 
-# Exemple d'utilisation
-url_page = input("Veuillez entrer l'URL de votre page : ")
-resultat = verifier_lien_internet(url_page)
-print(f"Résultat du test : {resultat}")
+def recup_page(url):
+    # Obtenir le contenu de la page
+    response = requests.get(url)
+    if response.status_code != 200:
+        print(f"Erreur lors de la récupération de la page. Code d'erreur : {response.status_code}")
+    return response
+
+def test_recup_page():
+    # URL de test
+    test_url = "https://www.example.com"
+    
+    # Appel de la fonction avec l'URL de test
+    result = recup_page(test_url)
+    
+    # Vérification que le résultat est un objet Response
+    assert isinstance(result, requests.Response)
+    
+    # Vérification du code d'état HTTP
+    assert result.status_code == 200
+
+
+
+def calculer_hachage(data):
+    sha256_hash = hashlib.sha256()
+    sha256_hash.update(data.encode('utf-8'))
+    return sha256_hash.hexdigest()
+
+def test_calculer_hachage():
+    # Données d'entrée
+    data = "Hello, World!"
+
+    # Résultat attendu
+    expected_hash = "dffd6021bb2bd5b0af676290809ec3a53191dd81c7f70a4b28688a362182986f"
+
+    # Calcul du hachage
+    result = calculer_hachage(data)
+
+    # Vérification
+    assert result == expected_hash
